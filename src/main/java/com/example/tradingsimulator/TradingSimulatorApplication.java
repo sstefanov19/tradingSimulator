bb29c46b2f2a72a8929d3a60fe43da4ce2b1f31d
sstefanov <84671344+sstefanov19@users.noreply.github.com>
2025-07-01 21:12:24 +0300

    init commit

---
+++ src/main/java/com/example/tradingsimulator/TradingSimulatorApplication.java
@@ -0,0 +1,13 @@
+package com.example.tradingsimulator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TradingSimulatorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TradingSimulatorApplication.class, args);
+    }
+
+}
